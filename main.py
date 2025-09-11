c988d9ce59bd7aee1e36d3696861f9b5de6338d7
gzl-shuai <1558507848@qq.com>
2025-09-11 15:53:19 +0800

    完成作业

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 WeigStr=input("请输入带有符号的重量值")
 if WeigStr.endswith('kg'):
     m=eval(WeigStr[:-2])*2.2046
-    print("转换后的重量是{:.2f}pd".format(m))
+    print("转换后的重量是{:.3f}pd".format(m))
 elif WeigStr.endswith('pd'):
     m=eval(WeigStr[:-2])/2.2046
-    print("转换后的重量是{:.2f}kg".format(m))
+    print("转换后的重量是{:.3f}kg".format(m))
 else:
     print("输入格式错误")# 在这个文件下编写代码，题目具体要求见README.md文件
